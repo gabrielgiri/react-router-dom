5e8860408e31dab4a5abb3d55bfafe75f27984f6
Gabriel Giri <gabrielgiri@gmail.com>
2022-10-24 18:58:29 -0300

    Added blur

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import './App.css';
 import { Element, Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
-const Winter = (): Element => <div className="seasons winter"><h1>Winter</h1></div>
-const Spring = (): Element => <div className="seasons spring"><h1>Spring</h1></div>
-const Summer = (): Element => <div className="seasons summer"><h1>Summer</h1></div>
-const Autumn = (): Element => <div className="seasons autumn"><h1>Autumn</h1></div>
+const Winter = (): Element => {
+  return (
+    <div className="seasons winter"><h1>Winter</h1>
+     <p>Winter is the coldest season of the year in polar and temperate climates. It occurs after autumn and before spring. The tilt of Earth's axis causes seasons; winter occurs when a hemisphere is oriented away from the Sun. Different cultures define different dates as the start of winter, and some use a definition based on weather.</p>
+    </div>
+  )
+}
 
+const Spring = (): Element => {
+  return (
+    <div className="seasons spring">
+      <h1>Spring</h1>
+      <p>Between winter and summer during which temperatures gradually rise.</p>
+      </div>
+  )
+}
+
+const Summer = (): Element => {
+  return (
+    <div className="seasons summer">
+      <h1>Summer</h1>
+      <p>The hottest of the four temperate seasons, occurring after spring and before autumn. At or centred on the summer solstice, the earliest sunrise and latest sunset occurs, daylight hours are longest and dark hours are shortest, with day length decreasing as the season progresses after the solstice.</p>
+      </div>
+  )
+}
+
+const Autumn = (): Element => {
+  return (
+    <div className="seasons autumn"><h1>Autumn</h1>
+     <p>Season of the year between summer and winter during which temperatures gradually decrease.</p>
+    </div>
+  )
+}
 
 function App(): Element {
   return (
@@ -14,7 +42,6 @@ function App(): Element {
 
       <div className="Parent">
         <div className="left">
-
 
           <header>
             <h1>react-router-dom</h1>
@@ -30,7 +57,6 @@ function App(): Element {
         </div>
         <div className="right">
 
-
           <Routes>
             <Route path="/winter-page" element={<Winter />} />
             <Route path="/spring-page" element={<Spring />} />
@@ -39,7 +65,6 @@ function App(): Element {
           </Routes>
         </div>
       </div>
-
 
       <div className="container">
 
