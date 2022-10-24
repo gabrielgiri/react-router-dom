5be327ec5f9e5f427b322d67aeedc625f282f437
Gabriel Giri <gabrielgiri@gmail.com>
2022-10-24 18:04:45 -0300

    Added readme

--- src/App.js
+++ src/App.js
@@ -2,50 +2,48 @@ import './App.css';
 import { Element, Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
-const Winter = () : Element => <div className="seasons winter"><h1>Winter</h1></div>
-const Spring = () : Element => <div className="seasons spring"><h1>Spring</h1></div>
-const Summer = () : Element => <div className="seasons summer"><h1>Summer</h1></div>
-const Autumn = () : Element => <div className="seasons autumn"><h1>Autumn</h1></div>
+const Winter = (): Element => <div className="seasons winter"><h1>Winter</h1></div>
+const Spring = (): Element => <div className="seasons spring"><h1>Spring</h1></div>
+const Summer = (): Element => <div className="seasons summer"><h1>Summer</h1></div>
+const Autumn = (): Element => <div className="seasons autumn"><h1>Autumn</h1></div>
 
 
-function App() : Element {
+function App(): Element {
   return (
     <div className="App">
 
-<div className="Parent">
+      <div className="Parent">
         <div className="left">
-          
-           
-            <header>
-      <h1>react-router-dom</h1>
-      <nav>
-        <ul>
-          <li><Link to = "/winter-page">Winter</Link></li>
-          <li><Link to = "/spring-page">Spring</Link></li>
-          <li><Link to = "/summer-page">Summer</Link></li>
-          <li><Link to = "/autumn-page">Autumn</Link></li>
 
 
-        </ul>
-      </nav>
-      </header>
+          <header>
+            <h1>react-router-dom</h1>
+            <nav>
+              <ul>
+                <li><Link to="/winter-page">Winter</Link></li>
+                <li><Link to="/spring-page">Spring</Link></li>
+                <li><Link to="/summer-page">Summer</Link></li>
+                <li><Link to="/autumn-page">Autumn</Link></li>
+              </ul>
+            </nav>
+          </header>
         </div>
         <div className="right">
-          
-          
-      <Routes>
-        <Route path = "/winter-page" element = {<Winter />} />
-        <Route path = "/spring-page" element = {<Spring />} />
-        <Route path = "/summer-page" element = {<Summer />} />
-        <Route path = "/autumn-page" element = {<Autumn />} />
-      </Routes>
+
+
+          <Routes>
+            <Route path="/winter-page" element={<Winter />} />
+            <Route path="/spring-page" element={<Spring />} />
+            <Route path="/summer-page" element={<Summer />} />
+            <Route path="/autumn-page" element={<Autumn />} />
+          </Routes>
         </div>
-    </div>
+      </div>
 
 
-       <div className="container">
+      <div className="container">
 
-    </div>
+      </div>
     </div>
   );
 }
